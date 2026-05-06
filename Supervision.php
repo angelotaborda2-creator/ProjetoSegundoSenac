6eb14d1085f91193b8c812cc16542bc388cca9cc
Angelo <angelotaborda2@gmail.com>
2026-05-06 20:31:45 -0300

    Dia 06/05/26

--- Supervision.php
+++ Supervision.php
@@ -3,7 +3,7 @@ $pageTitle = "OrbitaSenac - Supervision";
 include 'inc/header.inc.php'; 
 ?>
 <link rel="stylesheet" type="text/css" href="css/startup.css">
-<div class="startup"></div>
+<div class="startup">
     <h1>Sobre o Supervision</h1>
     <P>Saiba como nossa startup trabalha para oferecer o melhor da tecnologia.</p>
 
@@ -11,24 +11,42 @@ include 'inc/header.inc.php';
     <fieldset class="modelo">
         <legend>Conheça nossa Startup</legend>
         <input type="radio" name="modelo" value="preto" id="preto" checked>
-        <label for="preto"><img src="img/Supervision.png"></label>
-        <div class="texto">
-            <h3>Nossa Política</h3>
-            <p>
-                Técnicas: Scrum, Prototype <br>
-                Linguagens Front: HTML,CSS, JS <br>
-                Linguagens Back: PHP, Python, C# <br>
-                Framework Front: React, Bootstrap, Taiwind <br>
-                Framework Back: Laravel, Node <br>
-                Banco de Dados: Mysql, Postgres<br>
-            </p>
-        </div>
-        <input type="radio" name="modelo" value="azul" id="azul" checked>
-        <label for="azul"><img src="img/poge.png"></label>
-        <input type="radio" name="modelo" value="amarelo" id="amarelo" checked>
-        <label for="amarelo"><img src="img/pogerz.png"></label>
+        <label for="preto"><img src="img/Supervision.png">
+            <div class="texto">
+                <h3>Nossa Política</h3>
+                <p>
+                    Técnicas: Scrum, Prototype <br>
+                    Linguagens Front: HTML,CSS, JS <br>
+                    Linguagens Back: PHP, Python, C# <br>
+                    Framework Front: React, Bootstrap, Taiwind <br>
+                    Framework Back: Laravel, Node <br>
+                    Banco de Dados: Mysql, Postgres<br>
+                </p>
+            </div>
+        </label>
+        <input type="radio" name="modelo" value="azul" id="azul">
+        <label for="azul"><img src="img/poge.png">
+            <div class="texto">
+                <h3>Sobre o Dashboard</h3>
+                <p>A Dashdoard apresenta dados de produção de máquinas indústrias com inferencias de mercado e tempo para supervisores</p>
+            </div>
+        </label>
+        <input type="radio" name="modelo" value="amarelo" id="amarelo">
+        <label for="amarelo"><img src="img/pogerz.png">
+            <div class="texto">
+                <h3>Nossos Valores<h3>
+                    <p>
+                        Qualidade no Trabalho<br>
+                        Conetividade<br>
+                        Segurança<br>
+                    </p>
+            </div>
+        </label>
         
+        <div class="inscricao">
+            <input type="submit" value="Inscrição" name="inscricao">
+        </div>
     </fieldset>
-
 </form>
+</div>
 <?php include 'inc/footer.inc.php';?>
---
+++ inscrever.php
@@ -0,0 +1,1 @@
+oi tu tá inscrito
